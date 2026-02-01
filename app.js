504ff13628fa972bc74038c4b0c7043cecc7689c
codewithakshayp <paliwalakshay2013@gmail.com>
2026-02-01 19:22:50 +0530

    Initial website commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
+const input = document.querySelector("input");
 
 async function checkWeather(city){
     const response  = await fetch(apiUrl + city + `&appid=${apiKey}`);
